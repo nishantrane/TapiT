f3494085f538db41974ff64d41e09b6dae0df7ff
Nishant Rane <ranenishant31@gmail.com>
2017-12-09 13:05:08 +0530

    ng-hide

--- modules/getpin/getPinCtrl.js
+++ modules/getpin/getPinCtrl.js
@@ -1,8 +1,28 @@
 app.controller("getPinCtrl",function($scope,$location,store,$firebaseObject){
-	    
+	    $scope.show4pin=false;
+        $scope.hidemail=false;
+
+        $scope.setemail=function(){
+                
+                var str = $scope.EmailId;
+                store.email = str.replace(/\./g,"!");
+
+                store.database.child(store.email).set("");
+                $scope.show4pin=true;
+                $scope.hidemail=true;
+
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for (var i = 0; i < 10; i++)
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                console.log(text);
+                store.storage.setItem(3103,text);
+                //To Encrypt & decrypt use this key from local storage
+                //>>>>>>>>>>store.storage.getItem(3103)<<<<<<<<<<<<<<<
+        }
+
 	    $scope.getpinfunc=function(){
-            var str = $scope.EmailId;
-            store.email = str.replace(/\./g,"!");
 
             store.database.child(store.email).set("");
             if($scope.a==undefined && $scope.b==undefined && $scope.c==undefined && $scope.d==undefined){
