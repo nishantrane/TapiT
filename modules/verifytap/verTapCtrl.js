0b807291170a589c2914de6320815cbd2807c63d
Devesh Chatuphale <dcdevesh3@gmail.com>
2017-12-22 11:03:15 +0530

    updates in adddata controller

--- modules/verifytap/verTapCtrl.js
+++ modules/verifytap/verTapCtrl.js
@@ -1,33 +1,31 @@
 app.controller("verTapCtrl",function($scope,$location,store){
-		
+
 		$scope.v1=0;$scope.v2=0;$scope.v3=0;$scope.v4=0;
 		var temp='';
 		$scope.verifytapfunc=function(){
 			temp=temp+$scope.v1+$scope.v2+$scope.v2+$scope.v3;
 			var x=store.email;
-       		var dg=store.database.child(x).child("tapsCnt");
-            
-       		dg.on('value',function(sn){
+
+      var dg=store.database.child(x).child("tapsCnt");
+  		dg.on('value',function(sn){
 
         		if(temp==sn.val())
-				{
-				console.log(store.ks)
-				store.database.child(store.email).child('keyStr').set(store.ks);
-				console.log('TAPS | From firebase',sn.val());
-                console.log('TAPS | Entered by user',temp);	
+						{
+								console.log(store.ks)
+								store.database.child(store.email).child('keyStr').set(store.ks);
+								console.log('TAPS | From firebase',sn.val());
+                console.log('TAPS | Entered by user',temp);
                 alert(" Verified !");
-				$location.path("addpass");
-				
-				}
-
-             	else
-             	{
-             	console.log('TAPS | From firebase',sn.val());
-                console.log('TAPS | Entered by user',temp);	
-           		alert("Please try again");
-				$scope.v1=null;$scope.v2=null;$scope.v3=null;$scope.v4=null;
-            	}
+								$location.path("addpass");
+						}
+           	else
+            {
+             	  console.log('TAPS | From firebase',sn.val());
+                console.log('TAPS | Entered by user',temp);
+           		  alert("Please try again");
+								$scope.v1=null;$scope.v2=null;$scope.v3=null;$scope.v4=null;
+          	}
         	});
-			                 
+
 		};
 });
--- service/store.js
+++ service/store.js
@@ -1,5 +1,5 @@
 app.service('store', function($firebaseObject) {
-		//Testgitedit
+	
 		this.storage=window.localStorage;
 		this.lgflag=false;
 		this.keyary=[];
@@ -13,16 +13,14 @@ app.service('store', function($firebaseObject) {
 
 		this.encrypt= function(categorykey,pwd){
 			this.encryptedpwd = CryptoJS.AES.encrypt(pwd,this.key4ED); //encrypt ped using key4ED
-			//this.storage.setItem(categorykey,encryptedpwd); //store encr
 			console.log("Encrypted pass ",this.encryptedpwd);
 			return this.encryptedpwd;
-			//this.database.child(this.email).child(categorykey).set(encryptedpwd);	
+
 		}
 
 		this.decrypt= function(encrptpwd){
 			this.decryptedpwd = CryptoJS.AES.decrypt(encrptpwd,this.key4ED);
 			console.log("Decrypt ",this.decryptedpwd);
-			//decryptedpwd = CryptoJS.AES.decrypt(encrptpwd,this.key4ED);
 			return this.decryptedpwd.toString(CryptoJS.enc.Utf8);// converts to string and returns it 
 		}
 	});
